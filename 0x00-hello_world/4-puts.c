a79599900ebde83497a5e5ba08f857f504fdd726
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-08 20:57:07 -0700

    First Program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ * puts takes input string and display it
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
