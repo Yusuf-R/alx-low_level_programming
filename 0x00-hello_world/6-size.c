8a9f8d20a4f3f172f22eecf0b0712eadd7e584ec
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-09 07:17:30 -0700

    Size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int k;
 	float f;
 
-	printf("Size of a char: %zd byte(s)\n", sizeof(c));
-	printf("Size of an int : %zd byte(s)\n", sizeof(i));
-	printf("Size of a long int: %zd byte(s)\n", sizeof(j));
-	printf("Size of a long long int: %zd byte(s)\n", sizeof(k));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int : %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(j));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(k));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 	return (0);
