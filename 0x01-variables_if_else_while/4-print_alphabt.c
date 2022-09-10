f924b82147e884d9a319630f260fd20027bd8626
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-09 21:28:28 -0700

    Printing out all letters and excluding q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * program will display small case a to z
+ * using only putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
