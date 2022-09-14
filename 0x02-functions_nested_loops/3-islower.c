b2d3fe9acc9612c8a46c006524400887018e5e18
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-13 19:52:33 -0700

    Lower letter check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * int _islower - Entry point
+ * _islower - Entry point
+ * @c: is ASCII
  * Check if alphabet is lower
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{	
+	{
 		return (0);
 	}
 	else
 	{
-		return (1)
+		return (1);
 	}
 	_putchar('\n');
 }
