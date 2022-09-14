1ce2f757a760b014d915f3e1b396942f6edd9096
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-13 20:34:05 -0700

    Number checker

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: is ASCII
+ * Prints out the sighn of a number inpute
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	if (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	}
+	if (n < 0)
+	{
+		return (-1);
+		_putchar(45)
+	}
+
+	_putchar('\n');
+}
+
