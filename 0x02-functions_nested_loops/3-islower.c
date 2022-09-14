b0ed60e861f0983157119a6c5777372121d775f8
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-13 19:38:06 -0700

    Check lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _islower - Entry point
+ * Check if alphabet is lower
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{	
+		return (0);
+	}
+	else
+	{
+		return (1)
+	}
+	_putchar('\n');
+}
+
