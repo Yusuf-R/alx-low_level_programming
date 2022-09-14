ab7afa9e67484210c43dc92a2b36ca4a6dfe4f39
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-13 20:58:12 -0700

    Number checker

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n <= 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
@@ -23,5 +23,4 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	_putchar('\n');
-}
+	_putchar('\n');}
