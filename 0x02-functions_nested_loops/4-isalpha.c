e017d029289363bc1e49c29a63bc0cd6daaf34d4
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-13 20:00:28 -0700

    AlphaLower Check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _4-isalpha - Entry point
+ * @c: is ASCII
+ * Check if alphabet is lower
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 94 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
