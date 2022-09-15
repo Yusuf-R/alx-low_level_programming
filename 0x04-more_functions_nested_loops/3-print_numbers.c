b59d918b1fb5a5d5a031101bd0a3ed9d9dc27dc5
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-15 13:54:22 -0700

    Print out 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - To print out 0-9
+ * Return: 0 Success,  It returns the product of two numbers
+ */
+
+void print_numbers(void)
+{
+	int numb;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+		_putchar('\n');
+	}
+}
