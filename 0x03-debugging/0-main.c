0841b59a00c143d495fbaaf028af168d97c6389a
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-15 04:44:14 -0700

    First Debug Task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * This program test the function to print if
+ * and integer is positive or negative
+ * Retunrn - 0 for Success.
+ */
+int main (void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+In this section, we will test our function by giving it some 
+input in our program
+
+This concepts covers the Debugging process
