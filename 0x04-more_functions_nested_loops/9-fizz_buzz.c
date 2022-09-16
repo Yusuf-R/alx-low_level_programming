6c5960eccd5273f081c0b845bcefe77c315ab5ea
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-16 08:38:36 -0700

    Fizz Buzz Game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main  - To play the game of Fizz Buzz
+ * Return: 0 Success,  It returns the product of two numbers
+ */
+
+int main(void)
+{
+	int n_count;
+
+	for (n_count = 1; n_count <= 100; n_count++)
+	{
+		if ((n_count % 3 == 0) && (n_count % 5 == 0))
+		{
+			printf("Fizz Buzz ");
+		}
+		else if (n_count % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n_count % 5 == 0)
+		{
+			printf("Buzz ")
+		}
+		else
+			printf("%d ",n_count);
+	}
+	printf("\n");
+}
+
