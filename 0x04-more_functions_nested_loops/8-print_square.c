9a1f6d8071c672d84e8e68e1dbe411d5c94b288d
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-15 21:42:26 -0700

    Creating a square patterm

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - To print out a square of ##
+ * @size: an integer parameter for the square
+ * Return: 0 Success,
+ */
+
+void print_square(int size)
+{
+	int sq_rows;
+	int sq_cols;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	
+	for (sq_rows < 1; sq_rows <= size; sq_rows++)
+	{
+		for (sq_cols = 1; sq_cols <= size; sq_cols++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
