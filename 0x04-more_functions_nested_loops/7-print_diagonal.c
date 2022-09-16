845996d1f03c892ab6fa51dd2e39ed11dfcd5af0
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-15 19:04:15 -0700

    Print a diagonla

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - To print out any number or lines
+ * @n: integer parameter
+ * Return: 0 Success,
+ */
+
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar('\\');
+		_puthcar('\n')
+		n--;
+	}
+	_putchar('\n');
+}
