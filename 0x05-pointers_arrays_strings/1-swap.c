4df8d3d74550dc647addff7f36aa2a8e6a523196
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-19 23:42:41 +0100

    Swap Numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* swap_int - Entry point to our function. 
+* This function will swap the parameters to them selves
+* @a: a pointer variable for the first parameter
+* @b: a pointer variable for the second parameter
+* Return: 0. For Success
+*/
+
+void swap_int(int *a, int *b)
+{
+	int hold;
+	
+	hold  = *a;
+	*b = *a;
+	*a = hold;
+
+	return *a;
+	return *b;
+}
