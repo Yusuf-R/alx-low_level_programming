5e3334f32adab0e2fbec4d0f7656478f5a425da6
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-20 00:51:19 +0100

    Count length of any String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point to our function
+* @s: a pointer variable that takes and char
+* Return: 0. For Success
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++);
+	return (count);
+}
