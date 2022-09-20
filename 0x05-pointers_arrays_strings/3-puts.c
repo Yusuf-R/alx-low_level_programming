5a3720289375ea83c9b5b5db2fa292b1804621a9
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-20 01:36:30 +0100

    Puts to display the array

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - Entry point to our function
+* @str: a pointer variable that takes string
+* Return: 0. For Success
+*/
+
+void _puts(char *str)
+{
+	int counter;
+
+	for (counter = 0; str[counter] != '\0'; counter++)
+	{
+		_putchar(*(str + counter));
+	}
+}
