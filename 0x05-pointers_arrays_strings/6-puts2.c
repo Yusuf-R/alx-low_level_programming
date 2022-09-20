f7ab622ed6efe32f0589677dd843edd4f26a80bd
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-20 04:21:35 +0100

    Prints by skipping next element

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts2 - Entry point to our function
+* @str: a pointer variable that takes string
+* Return: 0. For Success
+*/
+
+void puts2(char *str);
+{
+	int counter = 0;
+
+	for (counter = 0; str[counter] != '\0'; counter = counter + 2)
+	{
+		_putchar(*(str + counter));
+	}
+	_putchar('\n');
+}
