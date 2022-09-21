d0696cccc0de78cce8a56ad3d4ecd9e374fb5732
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-21 03:20:50 +0100

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,8 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do {
+	do
+	{
 		if (*s == '-')
 			sign *= -1;
 
@@ -19,6 +20,8 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 
-	} while (*s++);
+	}
+	while (*s++);
 
-	return (num * sign);
+	return(num * sign);
+}
