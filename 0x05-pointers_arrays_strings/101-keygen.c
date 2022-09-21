013bcc8e3dd0061277495c8d1e7de9f9b8b1d6fb
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-22 00:52:10 +0100

    Random Password Generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - entry point
- *
- * Return: generated password
- */
+* main - entry point
+*
+* Return: genrated password
+*/
 
 int main(void)
 {
-	char c;
-	int x;
+	int length;
 
-	srand(time(0));
-	while (x <= 2645)
+	printf("Enter Length of your password: ");
+	scanf("%d", &length);
+	if (length <= 0)
 	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
+	printf("Password length must be >= 1!");
+	return (1);
 	}
-	putchar(2772 - x);
+	char *password = malloc(length + 1);
+	char *digits = "0123456789";
+	int digits_length = strlen(digits);
+	char *lowers = "abcdefghijklmnopqrstuwxyz";
+	int lowers_length = strlen(lowers);
+	char *uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int uppers_length = strlen(uppers);
+	char *symbols = "!@#$%^&*()";
+	int symbols_length = strlen(symbols);
 
+	srand(time(NULL) * getpid());
+
+	for (int i = 0; i < length; i++)
+	{
+	int char_type = rand() % 4;
+
+	if (char_type == 0)
+		password[i] = digits[rand() % digits_length];
+	else if (char_type == 1)
+		password[i] = lowers[rand() % lowers_length];
+	else if (char_type == 2)
+		password[i] = uppers[rand() % uppers_length];
+	else
+		password[i] = symbols[rand() % symbols_length];
+	}
+	password[length] = '\0';
+	printf("Password: %s\n", password);
+	free(password);
 	return (0);
 }
