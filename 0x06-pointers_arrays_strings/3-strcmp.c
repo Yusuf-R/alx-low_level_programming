21c122cbe131bf3965176d00b918b71cf7100f11
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-24 11:40:33 +0100

    My defined Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,21 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int a1;
+	int a2;
 
-	for (i = 0; s1[i] == s2[i]; i++)
+	for (i = 0; ; i++)
 	{
-		if (s1[i] == 0)
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
 			return (0);
+		}
+	a1 = (int) s1[i];
+	a2 = (int) s2[i];
+
+	if (a1 > a2)
+		return (a1 - a2);
+	if (a2 > a1)
+		return (a2 - a1);
 	}
-	return (s1[i] - s2[i]);
 }
