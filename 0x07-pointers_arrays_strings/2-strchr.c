fcdf44b674b7e3fd96eda6b6b68e43120fcecf3d
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-26 21:42:46 +0100

    Character Finder

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,19 +10,17 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i]; i++)
+	for (; ; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
 
 		else
-		if (s[i] == '\0')
+		if (*s == '\0')
 		{
-			break;
+			return (NULL);
 		}
 	}
 	return (NULL);
