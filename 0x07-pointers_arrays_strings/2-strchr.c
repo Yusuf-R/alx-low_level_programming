53a6aa97e2197d541912f30abc1780c6c43a803a
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-26 21:22:52 +0100

    Character Finder

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strchr - This function will find any instance of the seacrh charcter
+* @s: The pointer to the string
+* @c: The seach charcter of choice
+* Return: Returns O. for Success
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+		
+		else
+		if (s[i] == '\0')
+		{
+			return (NULL);
+		}
+	}
+
+	return (s);
+}
