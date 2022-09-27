5fad52b7dc5a5ea8d9b4e28bce42adbc19b944f9
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-27 20:48:54 +0100

    Double Pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* set_string -  This function will the value of the string
+* @s: The dereference value of to the string
+* @to: The seach charcter of choice
+* Return: Returns O. for Success
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
