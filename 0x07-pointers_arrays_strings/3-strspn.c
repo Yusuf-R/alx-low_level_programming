c45eada92633bb7a00081639aa75b758a509fd4b
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-27 02:05:46 +0100

    String Validator

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strspn - This function will find any instance of the seacrh charcter
+* @s: The pointer to the string
+* @accept: The seach charcter of choice
+* Return: Returns O. for Success
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ')
+			break;
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+		}
+	}
+
+	return (count);
+}
