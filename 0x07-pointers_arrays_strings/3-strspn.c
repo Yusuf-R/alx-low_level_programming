4b754b56f7519f5e94aa964f9fe68cfd27379775
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-27 02:08:26 +0100

    String Validator

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count;
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		if (s[i] == ' ')
 			break;
