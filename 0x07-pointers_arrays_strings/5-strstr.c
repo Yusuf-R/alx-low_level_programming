538368b4370a6ff96a48b26885ef0afc60510812
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-27 05:03:53 +0100

    String Match

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,30 +2,38 @@
 #include "main.h"
 
 /**
- * _strstr - a function that locates a substring
- * @haystack: the string to be tested
- * @needle: the substring to be searched for
- * Return: returns a pointer to the beginning of the
- * located substring, or NULL if the substring is not found
- */
+* _strstrk - This function is used  used to return the first
+* occurrence of the substring str2 in a string str1
+* @haystack: The pointer to the string to be scanned
+* @needle: The string containg the character to match
+* Return: returns a pointer to the located string, or
+* a null pointer if the string is not found
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j = 0, k;
+	int len1, len2;
+	int i, j;
 
-	for (i = 0; haystack[i] != 0; i++)
+	for (len1 = 0; haystack[len1] != '\0'; len1++)
 	{
-		k = i;
-		j = 0;
-		for (; needle[j] != 0; )
+	}
+	for (len2 = 0; needle[len2] != '\0'; len2++)
+	{
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		for (j = 0; j < len2; j++)
 		{
-			if (haystack[k++] == needle[j++])
+			if (haystack[i + j] != needle[j])
 			{
-				continue;
+				break;
 			}
-			break;
 		}
-		if (needle[j] == '\0')
-			return ((haystack + i));
+		if (!needle[j]
+			return (&haystack[i]);
+
 	}
 	return (NULL);
 }
