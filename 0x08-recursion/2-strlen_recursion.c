eccb543878d39dd4f0290a2af38396511f4c8993
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-29 02:07:41 +0100

    Recursion to print lenght of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -15,7 +15,7 @@ int _strlen_recursion(char *s)
 	src_len = 0;
 	if (*s != '\0')
 	{
-		return(src_len + 1) +  _strlen_recursion(s + 1);
+		return ((src_len + 1) +  _strlen_recursion(s + 1));
 	}
-	return(src_len);
+	return (src_len);
 }
