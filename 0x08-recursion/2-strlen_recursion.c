9c3917188845ad26d92f4307cffdf3fca9459591
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-29 01:56:17 +0100

    Recursion to print lenght of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - The function prints the lengh of a string
+*
+*@s: pointer to the string to be counted
+*
+*Return: Always 0.
+*/
+
+int _strlen_recursion(char *s)
+{
+	int src_len;
+	
+	src_len = 0;
+	if (*s != '\0')
+	{
+		src_len++;
+		src_len++ + _puts_recursion(s + 1);
+	}
+	else if (*s == '\0')
+	{
+		return(src_len);
+	}
+}
