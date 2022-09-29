442b92ec72d03c088bc908eed87843ec4218ce77
Yusuf-R <y.abdulwasiu@gmail.com>
2022-09-29 16:37:29 +0100

    Prime Number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+*is_prime_number - Function returns 1 or 0 if a number is prime.
+*
+*@n: number to check if it is a prime
+*
+*Return: Always 0.
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	return (prime_helper(n, 2));
+}
+
+/**
+*prime_helper - This function will help us get the prime numbere
+*@n: The square root number we need
+*@p_num: The number checker
+*Return: Return 1 or 0 depending on the sucess
+*/
+
+int prime_helper(int n, int p_num)
+{
+	if (n % p_num)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	p_num++;
+
+	return (prime_helper(n, p_num));
+}
