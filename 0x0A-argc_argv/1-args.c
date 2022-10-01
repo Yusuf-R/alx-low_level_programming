61b0289de94a9326671982e26f364fdd9337306b
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-01 01:06:54 +0100

    T2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -18,7 +18,7 @@
 *Return: 0 if successful
 */
 
-int main(int argc, char *argv[]) __attribute__((unused))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
