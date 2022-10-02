9de1edaca1fa596bea1d0f01deee2dc2d2550196
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-02 01:46:52 +0100

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,29 @@
 #include <ctype.h>
 
 /**
+*digit_check - This program demonstrates the argument to main
+*
+*@p: checks if the number is valid.
+*
+*Return: 0 if successful, 1 if Error
+*/
+
+int digit_check(char *p)
+{
+	int i = 0;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] < '0' || p[i] > '9')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+
+/**
 *main - This program demonstrates the argument to main
 *
 *@argc: Called ARGumentCount, it counts the arguments
@@ -29,7 +52,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			if (digit_check(argv[i]) == 1)
 			{
 				printf("Error\n");
 				return (1);
