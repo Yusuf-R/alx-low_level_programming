542eaa45d61377886f9cb75c3f838f7c396f8338
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-03 03:12:19 +0100

    Corrected 0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -25,7 +25,7 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 		if (accept[j] == '\0')
-			return (counter);
+			return (count);
 	}
 
 	return (count);
