72198bfa98119c6f071dedabf9840582d8b9bc13
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-06 23:20:18 +0100

    String Cat with n bytes

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat(NULL,"School !!!", 3);
+    concat = string_nconcat("Betty ","School !!!", 3);
     printf("%s\n", concat);
     free(concat);
     return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= len2)
 		n = len2;
-	memory = malloc(sizeof(*memory) * (len1 + n + 1));
+	memory = malloc(sizeof(char) * (len1 + n + 1));
 	if (memory == NULL)
 		return (NULL);
 	while (s1[i] && s1)
