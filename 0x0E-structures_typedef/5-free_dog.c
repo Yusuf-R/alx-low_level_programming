e1278c9671ce498273a4066ef4ce2e65d4b8c42e
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-11 03:37:24 +0100

    Free memory

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * Free_dog - function to the memory
+ * free_dog - function to the memory
  * @d: pointer d
  * Return: no return.
  */
@@ -9,7 +9,7 @@
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-	{	
+	{
 		return;
 	}
 	free(d->name);
