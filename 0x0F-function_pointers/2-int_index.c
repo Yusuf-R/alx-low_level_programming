08f966c107595078ddcfab9c8fcadf3a588e75d5
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-12 23:43:04 +0100

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,8 @@
 *@size: size of the pointr array
 *@cmp: the pointer to the function
 *
-*Return: Nothing.
+*Return: the index of first positive march
+*Else, -1.
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -16,10 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (!array || !cmp || size <= 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
-			return (i);		
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
