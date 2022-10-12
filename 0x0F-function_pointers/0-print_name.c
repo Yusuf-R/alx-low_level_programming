805acdc3a5011eb98ac861cea1794f3b1dcb94f4
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-12 19:20:06 +0100

    Print a name using a pointer function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+*print_name - The function prints a name
+*
+*@name: pointer to the name of the person
+*@f: the function pointer
+*Return: Nothing.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
