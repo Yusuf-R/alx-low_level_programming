d144ffb761c57abb8b039cc8c8b6fad4d1d4bdc2
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-13 04:14:38 +0100

    Task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,9 @@
-#ifndef _3_CALC_H
-#define _3_CALC_H
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct op - Struct op
@@ -7,7 +11,6 @@
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -19,8 +22,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-
 int (*get_op_func(char *s))(int, int);
 
 #endif
