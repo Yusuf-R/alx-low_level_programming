550a87cad70f9f0e513c726e507b599784c1f2cd
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-13 22:39:27 +0100

    Print Parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list argz;
 
-	if (!n)
+	if (n <= 0)
 		return;
 	va_start(argz, n);
 	if (separator == NULL)
