55c047c83e0036ba8ea44cee76b6dff2e47d7c25
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-24 22:27:27 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,17 @@
 
 void free_listint(listint_t *head)
 {
-	if (head != NULL)
-	{
-		if (head->next != NULL)
-			free(head->next);
-		free_listint(head->next);
-		free(head);
-	}
+	listint_t *tmp;
 
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		listint_t *aux = tmp;
+
+		tmp = tmp->next;
+
+		free(aux);
+	}
+	head = NULL;
 }
