e6da47cedaec8a7847e70583e355a9695df9f743
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-25 14:10:58 +0100

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = tmp;
 		return (tmp);
 	}
-	for (cnt = 1, trv = *head; trv != NULL && cnt <= idx; trv = trv->next, cnt++)
+	for (cnt = 0, trv = *head; trv != NULL && cnt <= idx; trv = trv->next, cnt++)
 	{
 		if (cnt == idx)
 		{
