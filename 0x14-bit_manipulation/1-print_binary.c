e7db84aa49d2145ae083f0406311da80fae6fe68
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-27 23:11:13 +0100

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - Function converts string to binary
+ * @n: pointer to the sring
+ * Return: return an unsigned int of binary conversion
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long bit;
+
+	if (n == 0)
+		_putchar(48);
+	else
+		for (bit = 1u << 31; bit != 0; bit >>= 1)
+		{
+			if (n & bit)
+			{
+				while (bit)
+				{
+					_putchar((n & bit) ? '1' : '0');
+					bit >>= 1;
+				}
+			}
+		}
+}
