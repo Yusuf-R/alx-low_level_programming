fa69d23d709bb280b11686ef585dc435ee239599
Yusuf-R <y.abdulwasiu@gmail.com>
2022-10-28 00:03:07 +0100

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - returns the value of the set bit
+ * @n: The actual num in dec
+ * @index: The given index positon to find it value
+ * Return: bit at the given index
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bin_index;
+
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+	bin_index = 1 << index;
+	*n = (bin_index | *n);
+	return (1);
+}
