99de135b111ada2a961da8e7a8a32442a1f0ab29
Yusuf-R <y.abdulwasiu@gmail.com>
2022-11-01 05:13:49 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[b])
+		while (text_content[cnt])
 			cnt++;
 		wr_fd = write(op_fd, text_content, cnt);
 		if (wr_fd != cnt)
