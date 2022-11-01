3e7fee03e1d7dbceca7d66ccd3a2a6d06801abe1
Yusuf-R <y.abdulwasiu@gmail.com>
2022-11-01 05:10:22 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	op_fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (op_fd < 0)
 		return (-1);
 
 	if (text_content)
