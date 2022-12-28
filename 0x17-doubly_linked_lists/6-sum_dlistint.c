4a6a711068fd1d2ca0e4a0d74b62ce669eea6d2b
Yusuf-R <y.abdulwasiu@gmail.com>
2022-12-28 17:56:36 +0100

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,26 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - sums all of the data of a dlistint_t linked list
- * @head: pointer to the beginning of the linked list
+ * get_dnodeint_at_index - get the nth node of a D-LL
  *
- * Return: sum of all data, or 0 if the list is empty
+ * @head: the poniter the start node
+ * @index: the given index position of the node
+ *
+ * Return: the pointer to that given node found at the index
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
+	dlistint_t *trav;
+	int sum;
 
-	while (head != NULL)
+	if (!head)
+		return (0);
+	trav = head;
+	sum = 0;
+	while (trav)
 	{
-		sum += head->n;
-		head = head->next;
+		sum += trav->n;
+		trav = trav->next;
 	}
 	return (sum);
 }
