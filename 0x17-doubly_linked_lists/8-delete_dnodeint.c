86cf9631412687bf5f1931b4b1e3603ce7678784
Yusuf-R <y.abdulwasiu@gmail.com>
2022-12-29 23:44:52 +0100

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,40 +1,72 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes a node at a specific index
- * @head: double pointer to the linked list
- * @index: index at which to delete node
+ * del_head - delete the node if head->next is valid
  *
- * Return: 1 on success, -1 on failure
+ * @head: double pointer to the start node
+ *
+ * Return: Nothing
  */
+void del_head(dlistint_t **head)
+{
+	dlistint_t *curr;
+
+	curr = *head;
+	*head = (*head)->next;
+	free(curr);
+	curr = NULL;
+	(*head)->prev = NULL;
+}
+
+/**
+ * delete_dnodeint_at_index - delete the node at the index position
+ *
+ * @head: double pointer to the start node
+ * @index: index position of the node
+ *
+ * Return: 1 for sucess or -1 for failure
+ */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
+	dlistint_t *curr, *prev;
+	unsigned int count;
 
-	if (head == NULL || *head == NULL)
+	if (!head || !(*head))
 		return (-1);
-	current = *head;
-	if (index == 0)
+
+	count = 0, curr = *head;
+	if (index == 0 && !(*head)->next)
 	{
-		*head = current->next;
-		if (current->next != NULL)
-		{
-			current->next->prev = NULL;
-		}
-		free(current);
+		free(*head), *head = NULL;
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	if (index == 0 && (*head)->next)
 	{
-		if (current->next == NULL)
-			return (-1);
-		current = current->next;
+		del_head(head);
+		return (1);
 	}
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-	free(current);
-	return (1);
+	while (curr)
+	{
+		prev = curr, curr = curr->next,	count++;
+		if (index == count && curr != NULL)
+		{
+			if (curr->next != NULL)
+			{
+				prev->next = curr->next;
+				curr->next->prev = prev;
+				free(curr), curr = NULL;
+				return (1);
+			}
+			else if (curr->next == NULL)
+			{
+				prev->next = NULL;
+				free(curr), curr = NULL;
+				return (1);
+			}
+		}
+		if (index == count && curr == NULL)
+			return (-1);
+	}
+	return (-1);
 }
