3b78e4873857df92b1d49e228a4963387213f4a8
Yusuf-R <y.abdulwasiu@gmail.com>
2022-12-30 07:31:53 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,22 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - find the culprit that causes the loop
+ * find_listint_loop - find the  loop in the link list
  *
- * @head: the pointer
+ * @head: the pointer to the start of the list
  *
- * Return: return the address of the loop causing node
+ * Return: the address of the node where the loop starts
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	if (head)
-	{
-		if (head > head->next)
-			head = find_listint_loop(head->next);
-		else
-			return (head->next);
-		return (head);
-	}
-	return (NULL);
+    listint_t *snail, *rabbit;
+
+    snail = rabbit = head;
+    if (!head)
+        return (NULL);
+    while(snail && rabbit && rabbit->next)
+    {
+        rabbit = rabbit->next->next;
+        snail = snail->next;
+        if (snail == rabbit)
+            break;
+    }
+    if (rabbit != snail || !rabbit || !snail || !rabbit->next)
+        return (NULL);
+    snail = head;
+    while(snail != rabbit)
+    {
+        snail = snail->next;
+        rabbit = rabbit->next;
+    }
+    return (rabbit);
 }
