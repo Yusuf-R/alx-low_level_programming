2c5b0a64740f3730379600cced9543050f449d77
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-13 18:43:05 +0100

    feat: :sparkles: a function def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,37 @@
-#!/usr/bin/python3
-"""
-Module for island_perimeter() method
-"""
-
-
 def island_perimeter(grid):
     """
-    Computers the length of the perimeter of an island.
+    Returns the perimeter of the island described in grid.
+
+    grid: a list of list of integers:
+            0 represents a water zone
+            1 represents a land zone
+            One cell is a square with side length 1
+            Grid cells are connected horizontally/vertically
+            (not diagonally).
+            Grid is rectangular, width and height don’t exceed 100
+            Grid is completely surrounded by water, and there is one island
+            (or nothing).
+            The island doesn’t have “lakes” (water inside that isn’t connected
+            to the water around the island).
     """
-    ret = 0
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == 1:
-                if y == 0 or grid[y - 1][x] == 0:
-                    ret += 1
-                if y == len(grid) - 1 or grid[y + 1][x] == 0:
-                    ret += 1
-                if x == 0 or grid[y][x - 1] == 0:
-                    ret += 1
-                if x == len(row) - 1 or grid[y][x + 1] == 0:
-                    ret += 1
-    return ret
+    # Get the dimensions of the grid
+    n_rows = len(grid)
+    n_cols = len(grid[0])
+
+    # Initialize the perimeter to 0
+    perimeter = 0
+
+    # Loop over each cell in the grid
+    for i in range(n_rows):
+        for j in range(n_cols):
+            # If the cell is land, add 4 to the perimeter
+            if grid[i][j] == 1:
+                perimeter += 4
+                # if there is land to the left and subtract 2 from the p
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+                # Check if there is land above and subtract 2 from the p
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
