0e9723d562b0b7972981a0f22fb4797cbcb4abe0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-13 06:46:06 +0100

    fix: :bug: fix the omission of commas

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -27,7 +27,7 @@ int binary_search(int *array, size_t size, int value)
 			if (i == r_idx)
 				printf("%d\n",  array[i]);
 			else
-				printf("%d ", array[i]);
+				printf("%d, ", array[i]);
 		}
 		if (array[m_idx] < value)
 			l_idx = m_idx + 1;
