a5cfc1ec61a990c6d66eb3af1145509dce3feafd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-13 06:34:12 +0100

    feat: :sparkles: using binary serch to find the index of a data

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,40 @@
 #include "search_algos.h"
 
+
 /**
- * binary_search - search for an integer using BSA
- * @array: pointer to first element of array
- * @size: size of the array
- * @value: value to seach
- *
- * Return: index location or -1
+ * binary_search - searches for a value in an array of integers
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value, -1 if not found
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start = 0, end = size - 1, mid;
-	size_t j;
+	size_t l_idx = 0;
+	size_t r_idx = size - 1;
+	size_t m_idx, i;
+	char *msg = "Searching in array";
 
 	if (!array || !size)
 		return (-1);
 
-	while (start <= end)
+	while (l_idx <= r_idx)
 	{
-		mid = (start + end) / 2;
-		printf("Searching in array:");
-		for (j = start; j <= end; j++)
+		m_idx = (l_idx + r_idx) / 2;
+		printf("%s: ", msg);
+		for (i = l_idx; i <= r_idx; i++)
 		{
-			if (j < end)
-				printf(" %d,", array[j]);
+			if (i == r_idx)
+				printf("%ld\n",  i);
 			else
-				printf(" %d\n", array[j]);
+				printf("%ld, ", i);
 		}
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] > value)
-			end = mid - 1;
+		if (array[m_idx] < value)
+			l_idx = m_idx + 1;
+		else if (array[m_idx] > value)
+			r_idx = m_idx - 1;
 		else
-			start = mid + 1;
+			return (m_idx);
 	}
 	return (-1);
 }
